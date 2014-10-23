67a517e421064f17c6dd22ec4c5d802576ca12a7
Luke Cooper <luke.cooper96@gmail.com>
2014-10-23 13:35:16 -0500

    review

--- review.py
+++ review.py
@@ -18,3 +18,6 @@
 # Remember how range can be used to create the list [2,4,6,8]
 # When complete, commit to github and submit a pull request!
 
+aList =[2,4,6,8,"Who do we appreciate"]
+for x in aList:
+    print x
